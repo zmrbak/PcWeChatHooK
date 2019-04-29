b1a77c5d40dcac6ba41c90236881ef4f1726b18e
庆明 赵 <20995658@qq.com>
2019-04-29 15:32:58 +0800

    创建

---
+++ L006CsHook/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L006CsHook
+{
+    public class Class1
+    {
+        public void ShowMessage()
+        {
+            MessageBox.Show("C# Hook OK", "C# OK");
+        }
+    }
+}

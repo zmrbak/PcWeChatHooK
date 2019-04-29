a5a54aac3dfab72233dfd23260481ba4d9b88fd7
庆明 赵 <20995658@qq.com>
2019-04-29 20:25:41 +0800

    L018：微信二维码解码及编码

---
+++ PcWeChatHooK/L018WeChatQrcodeDecodeEncode/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace L018WeChatQrcodeDecodeEncode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 21);
+            this.textBox1.TabIndex = 8;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(7, 271);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(169, 176);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(7, 79);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(169, 176);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(355, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(295, 429);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "微信截图";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PcWeChatHooK/L018WeChatQrcodeDecodeEncode/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.QrCode;
+
+namespace L018WeChatQrcodeDecodeEncode
+{
+    public partial class Form1 : Form
+    {
+        //微信进程
+        Process WxProcess = null;
+        Boolean Init()
+        {
+            WxProcess = null;
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                if (process.ProcessName == "WeChat")
+                {
+                    WxProcess = process;
+                    break;
+                }
+            }
+            if (WxProcess == null)
+            {
+                MessageBox.Show("微信没有找到！");
+                return false;
+            }
+            return true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (Init() == false) return;
+
+            //   1）获取设备上下文句柄：GetWindowDC-- > ReleaseDC
+            IntPtr windowDCHandle = GetWindowDC(IntPtr.Zero);
+            if (windowDCHandle == IntPtr.Zero)
+            {
+                MessageBox.Show("获取设备上下文句柄失败！");
+                return;
+            }
+
+            //   2）获取指定窗口边界和尺寸：GetWindowRect，
+            Rectangle rectangle = new Rectangle();
+            if (GetWindowRect(WxProcess.MainWindowHandle, ref rectangle) == 0)
+            {
+                MessageBox.Show("获取指定窗口边界和尺寸失败！");
+                return;
+            };
+
+            //	注意C#中的Rectangle与C++中RECT区别
+            //3）计算窗口大小
+            int width = rectangle.Width - rectangle.X;
+            int height = rectangle.Height - rectangle.Y;
+
+            //   4）创建一个设备上下文相关的位图：CreateCompatibleBitmap->DeleteObject
+            IntPtr compatibleBitmapHandle = CreateCompatibleBitmap(windowDCHandle, width, height);
+            if (compatibleBitmapHandle == IntPtr.Zero)
+            {
+                MessageBox.Show("创建一个设备上下文相关的位图失败！");
+                return;
+            }
+
+            //   5）创建一个内存上下文兼容的句柄：CreateCompatibleDC->DeleteDC
+            IntPtr compatibleDCHandle = CreateCompatibleDC(windowDCHandle);
+            if (compatibleDCHandle == IntPtr.Zero)
+            {
+                MessageBox.Show("创建一个内存上下文兼容的句柄失败！");
+                return;
+            }
+
+            //   6）选择一个设备上下文对象：SelectObject
+            if (SelectObject(compatibleDCHandle, compatibleBitmapHandle) == IntPtr.Zero)
+            {
+                MessageBox.Show("选择一个设备上下文对象失败！");
+                return;
+            }
+
+            //   7）拷贝窗口到设备上下文：PrintWindow
+            if (PrintWindow(WxProcess.MainWindowHandle, compatibleDCHandle, 0) == 0)
+            {
+                MessageBox.Show("拷贝窗口到设备上下文失败！");
+                return;
+            }
+
+            this.pictureBox1.Width = width;
+            this.pictureBox1.Height = height;
+            this.pictureBox1.Image = Image.FromHbitmap(compatibleBitmapHandle);
+
+            this.pictureBox1.Image.Save(Environment.CurrentDirectory + "\\Wx.png", ImageFormat.Png);
+
+            int qrWidth = 197;
+            int qrHeight = 197;
+
+            Image qrImage = new Bitmap(qrWidth, qrHeight);
+            Graphics graphics = Graphics.FromImage(qrImage);
+            graphics.DrawImage(Image.FromHbitmap(compatibleBitmapHandle),
+                new Rectangle(0, 0, qrWidth, qrHeight),
+                new Rectangle(40, 80, qrWidth, qrHeight),
+                GraphicsUnit.Pixel);
+            graphics.Dispose();
+
+            this.pictureBox2.Width = qrWidth;
+            this.pictureBox2.Height = qrHeight;
+            this.pictureBox2.Image = qrImage;
+
+
+            //   8）清理垃圾
+            DeleteObject(compatibleBitmapHandle);
+            DeleteDC(compatibleDCHandle);
+            ReleaseDC(WxProcess.MainWindowHandle, windowDCHandle);
+
+            IBarcodeReader reader = new BarcodeReader();
+            var barcodeBitmap = (Bitmap)(this.pictureBox1.Image);
+            var result = reader.Decode(barcodeBitmap);
+            if (result != null)
+            {
+                this.textBox1.Text = result.Text;
+            }
+
+            BarcodeWriter barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+
+            QrCodeEncodingOptions options = new QrCodeEncodingOptions();
+            options.Width = 150;
+            options.Height = 150;
+            options.Margin = 2;
+
+            barcodeWriter.Options = options;
+            Bitmap bitmap = barcodeWriter.Write(this.textBox1.Text);
+            this.pictureBox3.Width = 150;
+            this.pictureBox3.Height = 150;
+            this.pictureBox3.Image = bitmap;
+
+
+
+        }
+
+        [DllImport("Gdi32.dll")]
+        public static extern int DeleteDC(IntPtr hdc);
+
+        [DllImport("Gdi32.dll")]
+        public static extern int DeleteObject(IntPtr ho);
+
+        [DllImport("User32.dll")]
+        public static extern int PrintWindow(IntPtr hwnd, IntPtr hdcBlt, UInt32 nFlags);
+
+        [DllImport("Gdi32.dll")]
+        public static extern IntPtr SelectObject(IntPtr hdc, IntPtr h);
+
+        [DllImport("Gdi32.dll")]
+        public static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+
+        [DllImport("Gdi32.dll")]
+        public static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int cx, int cy);
+
+        [DllImport("User32.dll")]
+        public static extern int GetWindowRect(IntPtr hWnd, ref Rectangle lpRect);
+
+        [DllImport("User32.dll")]
+        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr hWnd);
+    }
+
+}

b365d48db23ba1dccbadfb54962ff269f2c1d94b
庆明 赵 <20995658@qq.com>
2019-04-29 20:34:45 +0800

    L021：编程读取微信中的PNG图片数据

---
+++ PcWeChatHooK/L021WeChatMemoryPngReader/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace L021WeChatMemoryPngReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.cbBassAddress = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(313, 334);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(230, 111);
+            this.textBox4.TabIndex = 21;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(313, 305);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(230, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "读取微信内存图片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.BtReadWxMemoryPng_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(470, 278);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(70, 21);
+            this.textBox5.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(382, 278);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(70, 21);
+            this.textBox3.TabIndex = 19;
+            // 
+            // cbBassAddress
+            // 
+            this.cbBassAddress.AutoSize = true;
+            this.cbBassAddress.Location = new System.Drawing.Point(487, 256);
+            this.cbBassAddress.Name = "cbBassAddress";
+            this.cbBassAddress.Size = new System.Drawing.Size(48, 16);
+            this.cbBassAddress.TabIndex = 17;
+            this.cbBassAddress.Text = "基址";
+            this.cbBassAddress.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(382, 254);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(70, 21);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.Text = "0x08624040";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(323, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "图片大小";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(323, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "微信内存";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(313, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 204);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(468, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "保存图片";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(388, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "选择图片";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "选择默认图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(290, 433);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 457);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.cbBassAddress);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox cbBassAddress;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PcWeChatHooK/L021WeChatMemoryPngReader/Form1.cs
@@ -0,0 +1,502 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L021WeChatMemoryPngReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //当前微信进程
+        Process WxProcess = null;
+        //微信WeChatWin基址
+        IntPtr WeChatWinBaseAddress = IntPtr.Zero;
+        //Image Byte
+        byte[] ImageByte = null;
+
+        /// <summary>
+        /// 在操作微信之前，确保微信已经在系统中运行
+        /// </summary>
+        /// <returns></returns>
+        bool IsWxRunning()
+        {
+            WxProcess = null;
+            //遍历所有进程
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                //如果进程名为“WeChat”，系统中有微信正在运行
+                if (process.ProcessName == "WeChat")
+                {
+                    //微信进程
+                    WxProcess = process;
+                    //在微信进程加载的模块中寻找“WeChatWin.dll”
+                    foreach (ProcessModule processModule in process.Modules)
+                    {
+                        if (processModule.ModuleName == "WeChatWin.dll")
+                        {
+                            //找到WeChatWin的基址
+                            WeChatWinBaseAddress = processModule.BaseAddress;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+
+            //如果系统中未找到运行的微信，则返回false
+            if (WxProcess == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 向文本框填入二进制文本数据
+        /// </summary>
+        /// <param name="pngBytes"></param>
+        private void SetTextBoxByBytes(byte[] pngBytes)
+        {
+            ImageByte = pngBytes;
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (byte item in pngBytes)
+            {
+                stringBuilder.Append(item.ToString("X2") + " ");
+            }
+            this.textBox1.Text = stringBuilder.ToString().TrimEnd();
+            this.textBox4.AppendText("数据长度：" + pngBytes.Length + Environment.NewLine);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Clear();
+            this.textBox4.Clear();
+            this.pictureBox1.Image = null;
+
+            String pngPath = "PngTest.png";
+            this.pictureBox1.ImageLocation = pngPath;
+
+            byte[] pngBytes = File.ReadAllBytes(pngPath);
+            SetTextBoxByBytes(pngBytes);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Clear();
+            this.textBox4.Clear();
+            this.pictureBox1.Image = null;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "*.png|*.png";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                String pngPath = openFileDialog.FileName;
+                this.pictureBox1.ImageLocation = pngPath;
+
+                byte[] pngBytes = File.ReadAllBytes(pngPath);
+                SetTextBoxByBytes(pngBytes);
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (this.pictureBox1.Image == null)
+            {
+                this.textBox4.AppendText("暂无数据，无法保存！");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "*.png|*.png";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllBytes(saveFileDialog.FileName, ImageByte);
+            }
+        }
+
+      
+        /// <summary>
+        /// 从微信内存中读取PNG图片数据，并显示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtReadWxMemoryPng_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Clear();
+            this.textBox4.Clear();
+            this.pictureBox1.Image = null;
+
+            if (IsWxRunning() == false)
+            {
+                this.textBox4.AppendText("微信没有找到！" + Environment.NewLine);
+                return;
+            }
+
+            //如果“基址”选择框被选择，文本框中的数字是图片的开始地址的地址
+            if (this.cbBassAddress.Checked == true)
+            {
+                //指针地址
+                int pointerAddress = int.Parse(
+                    this.textBox2.Text.Replace("0x", "").Replace("0X", ""),
+                    System.Globalization.NumberStyles.HexNumber
+                    );
+
+                //数据真实地址
+                Byte[] readStartAddress = GetWxDataBytes((IntPtr)(pointerAddress), 4);
+                if (readStartAddress == null)
+                {
+                    this.textBox4.AppendText("地址数据读取失败！" + Environment.NewLine);
+                    return;
+                }
+
+                int imageAddress = (int)(readStartAddress[0]) +
+                    (int)(readStartAddress[1] << 8) +
+                    (int)(readStartAddress[2] << 16) +
+                    (int)(readStartAddress[3] << 24);
+
+                //内存地址未4字节对齐
+                if (imageAddress % 4 != 0)
+                {
+                    this.textBox4.AppendText("指针指向的内存地址有误，内存地址必须为4的倍数！" + Environment.NewLine);
+                    this.textBox3.Text = "0x00000000";
+                    return;
+                }
+
+
+                //数据长度
+                int dataLength = int.Parse(
+                    this.textBox3.Text.Replace("0x", "").Replace("0X", ""),
+                    System.Globalization.NumberStyles.HexNumber
+                    );
+
+                //读取数据
+                Byte[] myBytes = GetWxDataBytes((IntPtr)(imageAddress), dataLength);
+
+                if (myBytes == null)
+                {
+                    this.textBox4.AppendText("未读取到数据！" + Environment.NewLine);
+                    return;
+                }
+
+                if (myBytes.Length < 8)
+                {
+                    this.textBox4.AppendText("读取的内容不足8位！" + Environment.NewLine);
+                    return;
+                }
+                if ((myBytes[0] != (byte)0x89) ||
+                    (myBytes[1] != (byte)0x50) ||
+                    (myBytes[2] != (byte)0x4E) ||
+                    (myBytes[3] != (byte)0x47) ||
+                    (myBytes[4] != (byte)0x0D) ||
+                    (myBytes[5] != (byte)0x0A) ||
+                    (myBytes[6] != (byte)0x1A) ||
+                    (myBytes[7] != (byte)0x0A))
+                {
+                    this.textBox4.AppendText("非PNG图片！" + Environment.NewLine);
+                    return;
+                }
+
+                //把数据显示到图片框
+                try
+                {
+                    MemoryStream memoryStream = new MemoryStream();
+                    memoryStream.Write(myBytes, 0, myBytes.Length);
+
+                    Image image = Image.FromStream(memoryStream);
+                    this.pictureBox1.Image = image;
+                }
+                catch
+                {
+                    this.textBox4.AppendText("图片无效！" + Environment.NewLine);
+                }
+                SetTextBoxByBytes(myBytes);
+            }
+            else
+            {
+                //设置每次读取的字节数
+                int readSize = 1024 * 10;
+                //每次读取到的内存数据，加入到列表中
+                List<byte[]> ReadMemoryList = new List<byte[]>();
+                //第几次读取内存
+                int readCount = 0;
+                //最大的读取次数
+                int readMaxCount = 1024;
+                //读取地址
+                int readStartAddress = int.Parse(this.textBox2.Text.Replace("0x", "").Replace("0X", ""), System.Globalization.NumberStyles.HexNumber);
+                //最近读取的数据中，有效数据数量
+                int lastByteLength = 0;
+                //末尾字符串
+                String iEndString = "0000000049454E44AE426082";
+                String tempIEND = "";
+                //循环读取内存
+                do
+                {
+                    //本次读取内存的地址
+                    int readAddress = readStartAddress + readSize * (readCount++);
+                    //判断是否达到做大的读取次数
+                    if (readCount >= readMaxCount)
+                    {
+                        this.textBox4.AppendText("已经达到最大读取次数，不再读取内存..." + Environment.NewLine);
+                        break;
+                    }
+
+                    //读取内存
+                    Byte[] myBytes = GetWxDataBytes((IntPtr)readAddress, readSize);
+
+                    //判断第一次读取是否有效
+                    if (readCount == 1)
+                    {
+                        //判断是否读取成功
+                        if (myBytes == null)
+                        {
+                            this.textBox4.AppendText("没有读取内容！" + Environment.NewLine);
+                            return;
+                        }
+
+                        //判断读取的数据是否有效
+                        if (myBytes.Length < 8)
+                        {
+                            this.textBox4.AppendText("读取的内容不足8位！" + Environment.NewLine);
+                            return;
+                        }
+                        if ((myBytes[0] != (byte)0x89) ||
+                            (myBytes[1] != (byte)0x50) ||
+                            (myBytes[2] != (byte)0x4E) ||
+                            (myBytes[3] != (byte)0x47) ||
+                            (myBytes[4] != (byte)0x0D) ||
+                            (myBytes[5] != (byte)0x0A) ||
+                            (myBytes[6] != (byte)0x1A) ||
+                            (myBytes[7] != (byte)0x0A))
+                        {
+                            this.textBox4.AppendText("非PNG图片！" + Environment.NewLine);
+                            return;
+                        }
+                    }
+
+                    //从第二次开始
+                    //检查读取内存数据是否成功
+                    if (myBytes == null)
+                    {
+                        this.textBox4.AppendText("PNG图片读取结束！" + Environment.NewLine);
+                        break;
+                    }
+                    //读取成功，添加到列表中
+                    ReadMemoryList.Add(myBytes);
+
+                    //判断本次是否读取到PNG图片的末尾数据IEND
+                    Boolean finished = false;
+                    for (int i = 0; i < myBytes.Length; i++)
+                    {
+                        lastByteLength = i;
+                        if (myBytes[i] == (byte)0)
+                        {
+                            tempIEND = "";
+                        }
+                        else
+                        {
+                            tempIEND += myBytes[i].ToString("X2");
+                            //找到PNG图片的末尾
+                            if ("00000000" + tempIEND == iEndString)
+                            {
+                                finished = true;
+                                this.textBox4.AppendText("到达PNG图片末尾！" + Environment.NewLine);
+                                break;
+                            }
+                        }
+                    }
+                    //找到PNG图片的末尾
+                    if (finished == true) break;
+                } while (true);
+                //图片数据大小
+                int bytelength = readSize * (ReadMemoryList.Count - 1) + lastByteLength;
+
+                //重新整理图片数据
+                byte[] data = new byte[bytelength];
+                //定义一个可变字符串
+                StringBuilder stringBuilder = new StringBuilder(bytelength * 3);
+
+                //把ReadMemoryList中的数据拼接到一个数组中
+                for (int i = 0; i < ReadMemoryList.Count - 1; i++)
+                {
+                    Array.ConstrainedCopy(ReadMemoryList[i], 0, data, readSize * i, readSize);
+                }
+                //最后一个数组
+                Array.ConstrainedCopy(ReadMemoryList[ReadMemoryList.Count - 1], 0, data, readSize * (ReadMemoryList.Count - 1), lastByteLength);
+
+                //把数据显示到图片框
+                try
+                {
+                    MemoryStream memoryStream = new MemoryStream();
+                    memoryStream.Write(data, 0, data.Length);
+                    Image image = Image.FromStream(memoryStream);
+                    this.pictureBox1.Image = image;
+                }
+                catch
+                {
+                    this.textBox4.AppendText("图片无效！" + Environment.NewLine);
+                }
+                SetTextBoxByBytes(data);
+            }
+        }
+
+        /// <summary>
+        /// 读取内存数据
+        /// </summary>
+        /// <param name="lpBaseAddress"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        Byte[] GetWxDataBytes(IntPtr lpBaseAddress, int length = 1024)
+        {
+            byte[] data = new byte[length];
+            if (ReadProcessMemory(WxProcess.Handle, lpBaseAddress, data, length, 0) == 0)
+            {
+                //读取内存失败！
+                return null;
+            }
+            return data;
+        }
+
+        [DllImport("Kernel32.dll")]
+        //BOOL ReadProcessMemory(
+        //  HANDLE hProcess,
+        //  LPCVOID lpBaseAddress,
+        //  LPVOID lpBuffer,
+        //  SIZE_T nSize,
+        //  SIZE_T* lpNumberOfBytesRead
+        //);
+        public static extern int ReadProcessMemory(
+           IntPtr hProcess, //正在读取内存的进程句柄。句柄必须具有PROCESS_VM_READ访问权限。
+           IntPtr lpBaseAddress,    //指向要从中读取的指定进程中的基址的指针。在发生任何数据传输之前，系统会验证基本地址和指定大小的内存中的所有数据是否都可以进行读访问，如果无法访问，则该函数将失败。
+           byte[] lpBuffer,  //指向缓冲区的指针，该缓冲区从指定进程的地址空间接收内容。
+           int nSize,    //要从指定进程读取的字节数。
+           int lpNumberOfBytesRead //指向变量的指针，该变量接收传输到指定缓冲区的字节数。如果lpNumberOfBytesRead为NULL，则忽略该参数。
+         );
+
+        /// <summary>
+        /// 点击复选框，变动数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateDataLength();
+        }
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            UpdateDataLength();
+        }
+        private void UpdateDataLength()
+        {
+            this.textBox1.Clear();
+            this.textBox4.Clear();
+            this.textBox5.Text = "0x" + 0.ToString("X8");
+
+            //this.pictureBox1.Image = null;
+
+            //确保微信运行中
+            if (IsWxRunning() == false)
+            {
+                this.textBox4.AppendText("微信没有找到！" + Environment.NewLine);
+                return;
+
+            }
+
+            int pointerAddress;
+            if (int.TryParse(
+                this.textBox2.Text.Replace("0x", "").Replace("0X", ""),
+                System.Globalization.NumberStyles.HexNumber,
+                NumberFormatInfo.InvariantInfo,
+                out pointerAddress)
+                == false)
+            {
+                this.textBox4.AppendText("内存地址非数字！" + Environment.NewLine);
+                return;
+            }
+
+            //内存地址未4字节对齐
+            if (pointerAddress % 4 != 0)
+            {
+                this.textBox4.AppendText("内存地址必须为4的倍数！" + Environment.NewLine);
+                this.textBox3.Text = "0x00000000";
+                return;
+            }
+
+            //检查复选框是否选择
+            if (cbBassAddress.Checked == false)
+            {
+                this.textBox3.Text = "0x00000000";
+                this.textBox5.Text = this.textBox2.Text;
+                return;
+            }
+
+            //图片数存放的地址
+            Byte[] readStartAddress = GetWxDataBytes((IntPtr)(pointerAddress), 4);
+            if (readStartAddress == null)
+            {
+                this.textBox4.AppendText("地址数据读取失败！" + Environment.NewLine);
+                return;
+            }
+
+            int imageAddress = (int)(readStartAddress[0]) +
+                (int)(readStartAddress[1] << 8) +
+                (int)(readStartAddress[2] << 16) +
+                (int)(readStartAddress[3] << 24);
+
+            //内存地址未4字节对齐
+            if (imageAddress % 4 != 0)
+            {
+                this.textBox4.AppendText("指针指向的内存地址有误，内存地址必须为4的倍数！" + Environment.NewLine);
+                this.textBox3.Text = "0x00000000";
+                return;
+            }
+
+            this.textBox5.Text = "0x" + imageAddress.ToString("X8");
+
+            //指针下一个Int，保存的是图片的长度
+            int lengthAddress = pointerAddress + 4;
+
+            //读出图片的长度
+            Byte[] byteLength = GetWxDataBytes((IntPtr)(lengthAddress), 4);
+            if (byteLength == null)
+            {
+                this.textBox3.Text = "0x00000000";
+                return;
+            }
+
+            //整理数据
+            int length = (int)(byteLength[0]) +
+                (int)(byteLength[1] << 8) +
+                (int)(byteLength[2] << 16) +
+                (int)(byteLength[3] << 24);
+
+            //使用Windows的mspaint画出的最小的Png图片(1像素)，大小为1052字节
+            //PNG头 IDHR长度0xF
+            //PMG末尾IEND长度0xC
+            if (length < 0xF + 0xC)
+            {
+                this.textBox4.AppendText("内存中图片长度无效（" + length + "）！" + Environment.NewLine);
+                this.textBox3.Text = "0x00000000";
+                return;
+            }
+
+
+            //将长度数据填写到文本框
+            this.textBox3.Text = "0x" + length.ToString("X8");
+        }
+    }
+}

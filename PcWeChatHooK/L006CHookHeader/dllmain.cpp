5db3aaad300bf20d2d71cda3f41066089a52f67e
庆明 赵 <20995658@qq.com>
2019-04-29 18:06:34 +0800

    创建完成

--- PcWeChatHooK/L006CHookHeader/dllmain.cpp
+++ PcWeChatHooK/L006CHookHeader/dllmain.cpp
@@ -1,12 +1,20 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "pch.h"
 
+using namespace L006CsHookDll;
+
+VOID Test()
+{
+	Class1^ class1 = gcnew Class1();
+	class1->ShowMessage();
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		MessageBox(NULL, TEXT("Hooked"), TEXT("OK"), MB_OK);
+		Test();
 		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
@@ -15,4 +23,3 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 	}
 	return TRUE;
 }
-

f07cf928f509a10c9aa09b477360e11d84a424a7
庆明 赵 <20995658@qq.com>
2019-04-29 21:17:52 +0800

    L027：用C#给微信编写一个WebServer

---
+++ PcWeChatHooK/L027WeChatWebServer/HttpServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L027WeChatWebServer
+{
+    public class HttpServer
+    {
+        public delegate void DataReceivedEventHandler(HttpListenerRequest reqeust, HttpListenerResponse response);
+        public event DataReceivedEventHandler OnDataReceived;
+
+        int port = 8421;
+        public int Port { get => port; set => port = value; }
+
+        public Boolean Start()
+        {
+            try
+            {
+                if (!HttpListener.IsSupported)
+                {
+                    Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                    return false;
+                }
+                HttpListener listener = new HttpListener();
+                listener.Prefixes.Add("http://+:"+ port + "/");
+                listener.Start();
+
+                listener.BeginGetContext(new AsyncCallback(ListenerCallback), listener);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.StackTrace);
+                return false;
+            }
+        }
+
+        private void ListenerCallback(IAsyncResult result)
+        {
+            HttpListener listener = (HttpListener)result.AsyncState;
+            HttpListenerContext context = listener.EndGetContext(result);
+            listener.BeginGetContext(new AsyncCallback(ListenerCallback), listener);
+            OnDataReceived(context.Request, context.Response);
+        }
+    }
+}
---
+++ PcWeChatHooK/L027WeChatWebServer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L027WeChatWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpServer httpServer = new HttpServer();
+            httpServer.OnDataReceived += HttpServer_OnDataReceived;
+
+            if (httpServer.Start())
+            {
+                Console.WriteLine("Web Server Started!");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void HttpServer_OnDataReceived(System.Net.HttpListenerRequest reqeust, System.Net.HttpListenerResponse response)
+        {
+            if (reqeust.HttpMethod == "POST")
+            {
+                Console.WriteLine("POST");
+                Stream stream = reqeust.InputStream;
+                BinaryReader binaryReader = new BinaryReader(stream);
+
+                byte[] data = new byte[reqeust.ContentLength64];
+                binaryReader.Read(data, 0, (int)reqeust.ContentLength64);
+
+                String dataString = Encoding.UTF8.GetString(data);
+                Console.WriteLine(dataString);
+
+
+            }
+            else
+            {
+                Console.WriteLine("NOT POST");
+
+            }
+
+
+            string responseString = "<HTML><BODY> Main:" + DateTime.Now + "</BODY></HTML>";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+        }
+    }
+}

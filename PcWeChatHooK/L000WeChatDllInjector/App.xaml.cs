ce3ce5a1e2041cad124d61d4f0bfb0dc98d60066
庆明 赵 <20995658@qq.com>
2019-04-30 11:31:54 +0800

    L000：微信DLL注入器，单实例运行

--- PcWeChatHooK/L000WeChatDllInjector/App.xaml.cs
+++ PcWeChatHooK/L000WeChatDllInjector/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,22 @@ namespace L000WeChatDllInjector
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //只运行一个实例
+            //把已经启动的实例，全部结束
+            Process thisProcess = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(thisProcess.ProcessName);
+
+            foreach (Process item in processes)
+            {
+                if (item.Id != thisProcess.Id)
+                {
+                    item.Kill();
+                }
+            }
+
+            base.OnStartup(e);
+        }
     }
 }

d1bfc50108a3f1795d2749275601fff0f8f6c98d
庆明 赵 <20995658@qq.com>
2019-05-03 19:53:36 +0800

    L022：【编程】以关闭Mutex的方式实现微信多开V0.0.2

--- PcWeChatHooK/L022CSMultiWeChat/Form1.Designer.cs
+++ PcWeChatHooK/L022CSMultiWeChat/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -44,7 +46,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 32);
+            this.button1.Location = new System.Drawing.Point(8, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -52,14 +54,37 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Git源代码";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "视频帮助";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(249, 98);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "微信多开器";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -70,6 +95,8 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- PcWeChatHooK/L022CSMultiWeChat/Form1.cs
+++ PcWeChatHooK/L022CSMultiWeChat/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -18,13 +19,28 @@ namespace L022CSMultiWeChat
             InitializeComponent();
         }
 
+        int i = 0;
         private void Button1_Click(object sender, EventArgs e)
         {
             int a = WeChatMultiOpen();
-            this.textBox1.AppendText(a + Environment.NewLine);
+            if (a == 0)
+            {
+                i++;
+                this.textBox1.AppendText("成功打开了"+i+"个微信！" + Environment.NewLine);
+            }
         }
 
         [DllImport("L022CMultiWeChat.dll")]
         public static extern int WeChatMultiOpen();
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/zmrbak/PcWeChatHooK");
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://t.cn/EXUbebQ");
+        }
     }
 }

6bbf52fe913f009a21e3cda573d69e630109f473
庆明 赵 <20995658@qq.com>
2019-05-30 20:57:18 +0800

    L047：【演示】IDA起步：从零开始进行软件分析WxLogOut

---
+++ PcWeChatHooK/L047WxLogOut/dllmain.cpp
@@ -0,0 +1,30 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include "Windows.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		HMODULE dllAdress = GetModuleHandleA("WeChatWin.dll");
+		DWORD callAdrress = (DWORD)dllAdress + 0x3F2A20;
+		HANDLE hANDLE = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)callAdrress, 0, NULL, 0);
+		if (hANDLE != 0)
+		{
+			CloseHandle(hANDLE);
+		}
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+

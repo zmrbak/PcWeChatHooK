b94219e804a0e1d1c81ed90f84bc43af3349bc1e
庆明 赵 <20995658@qq.com>
2019-05-31 15:19:01 +0800

    L048：【演示】IDA起步：从零开始WxQuitChatRoom

---
+++ PcWeChatHooK/L049_IDA_WxQuitChatRoom/dllmain.cpp
@@ -0,0 +1,72 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <Windows.h>
+
+VOID DoAction();
+//文本消息结构体
+struct StructWxid
+{
+	//发送的文本消息指针
+	wchar_t* pWxid;
+	//字符串长度
+	DWORD length;
+	//字符串最大长度
+	DWORD maxLength;
+
+	//补充两个占位数据
+	DWORD fill1 = 0;
+	DWORD fill2 = 0;
+};
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		//##########################################################
+		//
+		//注意：仅适配PC微信2.6.7.57版本，其它版本不可用
+		//
+		//##########################################################
+		HANDLE hANDLE = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)DoAction, NULL, NULL, 0);
+		if (hANDLE != 0)
+		{
+			CloseHandle(hANDLE);
+		}
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+VOID DoAction()
+{
+	WCHAR wxid[50] = L"4039017936@chatroom";
+	HMODULE dllAdress = GetModuleHandleA("WeChatWin.dll");
+	DWORD callAdrress = (DWORD)dllAdress + 0x265800;
+
+	//构建Wxid结构体
+	StructWxid structWxid = { 0 };
+	structWxid.pWxid = wxid;
+	structWxid.length = wcslen(wxid);
+	structWxid.maxLength = wcslen(wxid) * 2;
+
+	//取wxid的地址
+	DWORD* asmWxid = (DWORD*)& structWxid.pWxid;
+
+	__asm
+	{
+		mov ebx, asmWxid
+		push ebx
+		call callAdrress
+	}
+}
+

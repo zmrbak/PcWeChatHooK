b28ac628737d6b5941ce8ff5fd19f0690bfdb413
zmrbak <20995658@qq.com>
2019-09-04 20:46:36 +0800

    添加代码

---
+++ 配套代码/L067_LinkSample/Wx67/Wx67.cpp
@@ -0,0 +1,35 @@
+﻿// Wx67.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <list>
+#include<sstream>
+using namespace std;
+class MyClass
+{
+public:
+	int a = 0;
+	string b = "";
+};
+
+
+int main()
+{
+	list<MyClass> myList;
+	printf("%p", myList);
+
+	for (size_t i = 1; i < 5; i++)
+	{
+		MyClass myClass;
+		myClass.a = i;
+
+		stringstream ss;
+		string str;
+		ss << i;
+		ss >> str;
+		myClass.b = "String"+ str;
+		myList.push_back(myClass);
+	}
+
+    std::cout << "Hello World!\n";
+}

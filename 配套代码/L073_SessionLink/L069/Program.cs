c94d05e46258f337f271876762bd9baa7b212bd3
zmrbak <20995658@qq.com>
2019-09-14 15:36:39 +0800

    添加源码

---
+++ 配套代码/L073_SessionLink/L069/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace L069
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WxSessionList wxSessionList = new WxSessionList();
+            wxSessionList.GetData();
+
+            Console.ReadLine();
+            //Process.Start("https://github.com/zmrbak/PcWeChatHooK");
+        }
+    }
+}

00b4fff945b47e9190131b89f2f04e8a1dfe5635
zmrbak <20995658@qq.com>
2019-10-17 10:25:23 +0800

    上传源码

---
+++ 配套代码/L076_SQLite/L076/L076.cpp
@@ -0,0 +1,114 @@
+﻿// L076.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include"sqlite3.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int MyCallback(void* para, int nColumn, char** colValue, char** colName);//回调函数
+
+int main(int argc, char* argv[])
+{
+	sqlite3* db = NULL;
+	int result = sqlite3_open("test.db", &db);
+
+	if (result != SQLITE_OK)
+	{
+		printf("open database text.db failed \n");
+		return 0;
+	}
+	else
+	{
+		printf("open database text.db success \n");
+	}
+	////////开始执行sqlite
+	const char* sql = "create table Student(t_id integer primary key autoincrement, t_name varchar(15), t_age integer)";
+	char* errMsg = NULL;
+	result = sqlite3_exec(db,
+		sql,
+		NULL,
+		NULL,
+		&errMsg);
+	if (result != SQLITE_OK)
+	{
+		printf("create table Student failed\n");
+		printf("error conde %d \t error message:%s", result, errMsg);
+	}
+	//插入数据
+	errMsg = NULL;
+	sql = "insert into Student(t_name, t_age) values ('dwb', 23)";
+	result = sqlite3_exec(db,
+		sql,
+		NULL,
+		NULL,
+		&errMsg);
+	printf("insert message1:%s \n", errMsg);
+
+	errMsg = NULL;
+	sql = "insert into Student(t_name, t_age) values ('dhx', 25)";
+	result = sqlite3_exec(db,
+		sql,
+		NULL,
+		NULL,
+		&errMsg);
+	printf("insert message2:%s \n", errMsg);
+
+	errMsg = NULL;
+	sql = "insert into Student(t_name, t_age) values ('dwz', 21)";
+	result = sqlite3_exec(db,
+		sql,
+		NULL,
+		NULL,
+		&errMsg);
+	printf("insert message3:%s \n", errMsg);
+
+	////////////////////////////////////////////////////////
+	errMsg = NULL;
+	sql = "select * from Student;";
+	result = sqlite3_exec(db, sql, MyCallback, NULL, &errMsg);
+	printf("select message:%s \n", errMsg);
+
+	//执行不用回调函数的sql语句，先要设置函数所需的参数
+	printf("\nUSEING sqlite3_get_table()----------------------------\n");
+	sql = "select * from Student;";
+
+	int nCols;
+	int nRows;
+	char** azResult;
+	errMsg = NULL;
+	int index = 0;
+
+	result = sqlite3_get_table(db, sql, &azResult, &nRows, &nCols, &errMsg);
+	printf("result = %d \t errMsg = %s \n", result, errMsg);
+	printf("rows:%d \t cols: %d \n", nRows, nCols);
+	index = nCols;
+
+	printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+	for (int i = 0; i < nRows; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+		{
+			printf("%s::%s", azResult[j], azResult[index]);
+			index++;
+			printf("\n");
+		}
+	}
+	printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+
+	sqlite3_free_table(azResult);
+	sqlite3_close(db);
+
+	return 0;
+}
+
+int MyCallback(void* para, int nColumn, char** colValue, char** colName)
+{
+	printf("----------------------------------------------------\n");
+	printf("包含的列数：%d\n", nColumn);
+	for (int i = 0; i < nColumn; i++)
+	{
+		printf("%s :%s\n", *(colName + i), colValue[i]);//指针和数组的两种写法
+	}
+	printf("----------------------------------------------------\n");
+	return 0;
+}
---
+++ 配套代码/L078_SQLite_sqlite_exec/L078/dllmain.cpp
@@ -0,0 +1,109 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+void selectData();
+DWORD dllAdress = 0;
+
+typedef int (*sqlite3_callback)(void*, int, char**, char**);
+
+typedef int(WINAPI* Sqlite3_exec)(
+	DWORD,                /* The database on which the SQL executes */
+	const char*,           /* The SQL to be executed */
+	sqlite3_callback, /* Invoke this callback routine */
+	void*,                 /* First argument to xCallback() */
+	char**             /* Write error messages here */
+	);
+
+int MyCallback(void* para, int nColumn, char** colValue, char** colName);//回调函数
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		//##########################################################
+		//
+		//注意：仅适配PC微信2.6.8.65版本，其它版本不可用
+		//
+		//##########################################################
+
+		HANDLE hANDLE = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)selectData, 0, NULL, 0);
+		if (hANDLE != 0)
+		{
+			CloseHandle(hANDLE);
+		}
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+//开始InlineHook
+void selectData()
+{
+	//循环等待获取微信基址
+	while (TRUE)
+	{
+		dllAdress = (DWORD)GetModuleHandleA("WeChatWin.dll");
+		if (dllAdress == 0)
+		{
+			Sleep(100);
+		}
+		else
+		{
+			break;
+		}
+	}
+	////sqlite3_exec
+	//基址 78CF0000
+	//偏移：‭77F6C0‬
+	//SQLITE_API int sqlite3_exec(
+	//	sqlite3 * db,                /* The database on which the SQL executes */
+	//	const char* zSql,           /* The SQL to be executed */
+	//	sqlite3_callback xCallback, /* Invoke this callback routine */
+	//	void* pArg,                 /* First argument to xCallback() */
+	//	char** pzErrMsg             /* Write error messages here */
+	//)
+
+	//SQLITE_API int sqlite3_exec(
+	//	sqlite3*,                                  /* An open database */
+	//	const char* sql,                           /* SQL to be evaluated */
+	//	int (*callback)(void*, int, char**, char**),  /* Callback function */
+	//	void*,                                    /* 1st argument to callback */
+	//	char** errmsg                              /* Error msg written here */
+	//);
+
+	Sqlite3_exec sqlite3_exec;
+	sqlite3_exec = (Sqlite3_exec)(dllAdress + 0x77F6C0);
+
+	DWORD db = 0x03111D88;
+	const char* sql = "select * from sqlite_master";
+	char* errmsg = NULL;
+	DWORD i = sqlite3_exec(db, sql, MyCallback, NULL, &errmsg);
+}
+
+int MyCallback(void* para, int nColumn, char** colValue, char** colName)
+{
+	//OutputDebugStringA("----------------------------------------------------\n");
+
+	CHAR sOut[MAX_PATH + 11] = { 0 };
+	_snprintf_s(sOut, MAX_PATH + 11, "包含的列数：%d\n", nColumn);
+	//OutputDebugStringA(sOut);
+	MessageBoxA(NULL, sOut, "", MB_OK);
+
+	for (int i = 0; i < nColumn; i++)
+	{
+		CHAR sOut[1024] = { 0 };
+		_snprintf_s(sOut, 1024, "%s :%s\n", *(colName + i), colValue[i]);
+		//OutputDebugStringA(sOut);
+		MessageBoxA(NULL, sOut, "", MB_OK);
+	}
+	return 0;
+}
